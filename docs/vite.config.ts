151862e043483134d858333708527474dfbe6e79
itwangc <1105947676@qq.com>
2026-03-21 22:40:24 +0800

    更新文章和搜索组件

--- docs/vite.config.ts
+++
@@ -1,7 +0,0 @@
-// vite.config.ts
-import { defineConfig } from 'vite'
-import { searchIndexPlugin } from './scripts/search-index.js'
-
-export default defineConfig({
-  plugins: [searchIndexPlugin()],
-})
